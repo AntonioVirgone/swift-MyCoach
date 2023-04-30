bb30c4c8385773b5cf49f6047047dce99e3c45a1
antonio.virgone <antonio.virgone@wisemotions.com>
2023-04-30 13:52:29 +0200

    Initial Commit

---
+++ MyCoach/MyCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCoachApp.swift
+//  MyCoach
+//
+//  Created by Antonio Virgone on 30/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MyCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
